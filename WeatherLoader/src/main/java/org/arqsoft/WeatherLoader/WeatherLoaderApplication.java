0597658edc8f977d161633bd7b886e1d9c138f8a
Mauro <reekremag@gmail.com>
2023-06-07 18:00:52 -0300

    :package: agrego estructura base de WeatherLoader

---
+++ WeatherLoader/src/main/java/org/arqsoft/WeatherLoader/WeatherLoaderApplication.java
@@ -0,0 +1,13 @@
+package org.arqsoft.WeatherLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherLoaderApplication.class, args);
+	}
+
+}
---
+++ WeatherLoader/src/test/java/org/arqsoft/WeatherLoader/WeatherLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arqsoft.WeatherLoader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
