28701e751c7ff6e328f637be49074555ebbaa34f
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2023-06-23 23:46:43 -0300

    :sparkles: log aggregation con stack ELK
    Elastic Search para almacenamiento, Log Stash como compilador de informacion  y Kibana como UI
    
    Signed-off-by: Fede Sandoval <federico.sandoval@alu.unq.edu.ar>

--- WeatherMetric/build.gradle.kts
+++ WeatherMetric/build.gradle.kts
@@ -29,9 +29,10 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-	
-	implementation("io.zipkin.reporter2:zipkin-reporter-brave")
-implementation("io.micrometer:micrometer-tracing-bridge-brave:1.1.2")
+
+	implementation("net.logstash.logback:logstash-logback-encoder:4.11")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 
 }
 
--- WeatherMetric/src/main/kotlin/ar/edu/unq/weather/metric/infra/controller/CurrentWeatherSpringboot.kt
+++ WeatherMetric/src/main/kotlin/ar/edu/unq/weather/metric/infra/controller/CurrentWeatherSpringboot.kt
@@ -1,6 +1,9 @@
 package ar.edu.unq.weather.metric.infra.controller
 
 import ar.edu.unq.weather.metric.application.CurrentWeather
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,14 +14,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1")
 class CurrentWeatherSpringboot {
-
     @Autowired
     private lateinit var currentWeatherService: CurrentWeather
 
+    private val log: Logger = LoggerFactory.getLogger(CurrentWeatherSpringboot::class.java)
     @RequestMapping(value = ["/current"], method = [RequestMethod.GET])
     fun execute(): ResponseEntity<*>{
+        val res = this.currentWeatherService.execute()
+        this.log.info("attaching new weather {}", res)
         return ResponseEntity(
-                this.currentWeatherService.execute(),
+                res,
                 HttpStatus.OK
         )
     }
